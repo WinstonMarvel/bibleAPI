c1c533bccb273f5f6ccd6e88dd2a75d1555f8395
Marvel Jude <user.email>
2018-02-07 22:18:40 +0530

    Began work on middleware requestprocessor

--- app.js
+++ app.js
@@ -2,28 +2,23 @@
 const express = require("express");
 const app = express();
 
-const bible = require("./bibles/MSG.json");
+const requestParser = require("./requestParser.js");
 
-var responseJSON = {};
-
-app.get("/:book/:chapter/:verse", function(req,res){
-	responseJSON.book = bible[req.params.book][req.params.chapter][req.params.verse] || "Incorrect Book Name";
-	responseJSON.chapter = req.params.chapter || "Incorrect Book Name";
-	responseJSON.verse = req.params.verse || "Incorrect Book Name";
-	res.json(responseJSON);
-});
+app.get("/:book/:chapter/:verse", requestParser.process);
+app.get("/:book/:chapter", requestParser.process);
 
 app.get("*", (req,res)=>{
 	res.send("Incorrect format. Sorry please read the documentation.");
 });
 
-console.log(bible.Genesis);
+
 
 app.listen(3000);
+
 
 
 //To do
 	//Add Middleware function to process request:
 		// Bookname in camel case always
 		// Check !hasOwnProperty and return the appropriate response
-		//
+		// Error Handler
---
+++ requestParser.js
@@ -0,0 +1,19 @@
+const bible = require("./bibles/MSG.json");
+
+var processor = function(req, res){
+	let responseJSON = {};
+	console.log(typeof req.params.chapter);
+	console.log(bible[req.params.chapter]);
+	responseJSON.chapter = bible[req.params.book][req.params.chapter] || "Incorrect Chapter";
+	responseJSON.verse = bible[req.params.book][req.params.chapter][req.params.verse] || "Incorrect Verse Number";
+	responseJSON.success = true;
+	if(!bible[req.params.book] || !bible[req.params.chapter] || !bible[req.params.verse]){
+		responseJSON.success = false;
+		responseJSON.errorCode = "Incorrect Parameters";
+	}
+	res.json(responseJSON);
+}
+
+
+
+module.exports.process = processor;
